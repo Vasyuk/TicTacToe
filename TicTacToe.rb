d95714115b125950f02fba8bb265ed915c654774
Vasyuk <vasyuk@MacBook-Air-de-Vasyuk.local>
2017-07-25 15:09:32 +0200

    Version 1.0

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -22,84 +22,91 @@ class BoardCase
 end
 
 class Board
-  @@case1 = BoardCase.new
-    @@case1.bZ = " "
-  @@case2 = BoardCase.new
-    @@case2.bZ = " "
-  @@case3 = BoardCase.new
-    @@case3.bZ = " "
-  @@case4 = BoardCase.new
-    @@case4.bZ = " "
-  @@case5 = BoardCase.new
-    @@case5.bZ = " "
-  @@case6 = BoardCase.new
-    @@case6.bZ = " "
-  @@case7 = BoardCase.new
-    @@case7.bZ = " "
-  @@case8 = BoardCase.new
-    @@case8.bZ = " "
-  @@case9 = BoardCase.new
-    @@case9.bZ = " "
+  $case1 = BoardCase.new
+    $case1.bZ = " "
+  $case2 = BoardCase.new
+    $case2.bZ = " "
+  $case3 = BoardCase.new
+    $case3.bZ = " "
+  $case4 = BoardCase.new
+    $case4.bZ = " "
+  $case5 = BoardCase.new
+    $case5.bZ = " "
+  $case6 = BoardCase.new
+    $case6.bZ = " "
+  $case7 = BoardCase.new
+    $case7.bZ = " "
+  $case8 = BoardCase.new
+    $case8.bZ = " "
+  $case9 = BoardCase.new
+    $case9.bZ = " "
   def initBoard(name,bZe)
     if name == "1"
-      @@case1.bZ = bZe
+      $case1.bZ = bZe
     elsif name == "2"
-      @@case2.bZ = bZe
+      $case2.bZ = bZe
     elsif name == "3"
-      @@case3.bZ = bZe
+      $case3.bZ = bZe
     elsif name == "4"
-      @@case4.bZ = bZe
+      $case4.bZ = bZe
     elsif name == "5"
-      @@case5.bZ = bZe
+      $case5.bZ = bZe
     elsif name == "6"
-      @@case6.bZ = bZe
+      $case6.bZ = bZe
     elsif name == "7"
-      @@case7.bZ = bZe
+      $case7.bZ = bZe
     elsif name == "8"
-      @@case8.bZ = bZe
+      $case8.bZ = bZe
     elsif name == "9"
-      @@case9.bZ = bZe
+      $case9.bZ = bZe
     end
   end
   def showBoard
-      puts @@case1.bZ + " | " +  @@case2.bZ + " | " +  @@case3.bZ
-      puts "----------"
-      puts @@case4.bZ + " | " +  @@case5.bZ + " | " +  @@case6.bZ
-      puts "----------"
-      puts @@case7.bZ + " | " +  @@case8.bZ + " | "+  @@case9.bZ
+      puts " "
+      puts " "
+      puts $case1.bZ + " | " +  $case2.bZ + " | " +  $case3.bZ
+      puts "---------"
+      puts $case4.bZ + " | " +  $case5.bZ + " | " +  $case6.bZ
+      puts "---------"
+      puts $case7.bZ + " | " +  $case8.bZ + " | "+  $case9.bZ
+      puts " "
+      puts " "
   end
 end
 
 
-# class FindWinner
-#     win = false
-#   if @@case1.bZ == @@case2.bZ || @@case2.bZ  == @@case3.bZ
-#     win = true
-#   # elsif @@case4.bZ == @@case5.bZ == @@case6.bZ
-#   #   return true
-#   # elsif @@case7.bZ == @@case8.bZ == @@case9.bZ
-#   #   return true
-#   # elsif @@case1.bZ == @@case4.bZ == @@case7.bZ
-#   #   return true
-#   # elsif @@case2.bZ == @@case5.bZ == @@case8.bZ
-#   #   return true
-#   # elsif @@case3.bZ == @@case6.bZ == @@case9.bZ
-#   #   return true
-#   # elsif @@case1.bZ == @@case5.bZ == @@case9.bZ
-#   #   return true
-#   # elsif @@case3.bZ == @@case5.bZ == @@case7.bZ
-#   #   return true
-#   end
-#   return win
-# end
+class FindWinner
+   def youWon
+      win = false
+      if ($case1.bZ == $case2.bZ && $case2.bZ  == $case3.bZ)&&(!($case1.bZ == " "))
+        win = true
+      elsif ($case4.bZ == $case5.bZ && $case5.bZ == $case6.bZ)&&(!($case4.bZ == " "))
+        puts "Hello"
+        win = true
+      elsif ($case7.bZ == $case8.bZ && $case8.bZ == $case9.bZ)&&(!($case7.bZ == " "))
+        win = true
+      elsif ($case1.bZ == $case4.bZ && $case4.bZ == $case7.bZ)&&(!($case1.bZ == " "))
+        win = true
+      elsif ($case2.bZ == $case5.bZ && $case5.bZ == $case8.bZ)&&(!($case2.bZ == " "))
+        win = true
+      elsif ($case3.bZ == $case6.bZ && $case6.bZ == $case9.bZ)&&(!($case3.bZ == " "))
+        win = true
+      elsif ($case1.bZ == $case5.bZ && $case5.bZ == $case9.bZ)&&(!($case1.bZ == " "))
+        win = true
+      elsif ($case3.bZ == $case5.bZ && $case5.bZ == $case7.bZ)&&(!($case3.bZ == " "))
+        win = true
+      end
+      return win
+  end
+end
 
 class Game
+  @@findWinner = FindWinner.new
   @@player1 = Player.new
   @@player2 = Player.new
   @@board = Board.new
   def initialising
     puts "Welcome to the best game in the world \"Tic-Tac-Toe\" "
-
     puts "Player 1 write his name:"
     namePl = gets.chomp
     @@player1.name = @@player1.verificationName(namePl)
@@ -120,18 +127,42 @@ class Game
   end
 
   def gamee
-    for n in (0..4)
-      puts @@player1.name + " chooses where he wants to put " + @@player1.daggerZero + " 1 - 9"
-      digit = gets.chomp
-      @@board.initBoard(digit, @@player1.daggerZero)
-      @@board.showBoard
-      puts @@player2.name + " chooses where he wants to put " + @@player2.daggerZero + " 1 - 9"
-      digit = gets.chomp
-      @@board.initBoard(digit, @@player2.daggerZero)
-      @@board.showBoard
-    end
+      array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      name = nil
+      while !(@@findWinner.youWon == true)
+        if !(@@findWinner.youWon == true)
+            puts @@player1.name + " chooses where he wants to put " + @@player1.daggerZero + " 1 - 9"
+            digit = gets.chomp
+            while !(array.include? digit.to_i)
+              puts "This cell is already taken, choose other cell:"
+              digit = gets.chomp
+            end
+            while array.include? digit.to_i
+                @@board.initBoard(digit, @@player1.daggerZero)
+                @@board.showBoard
+                array.delete(digit.to_i)
+            end
+            name = @@player1.name
+        end
+        if !(@@findWinner.youWon == true)
+            puts @@player2.name + " chooses where he wants to put " + @@player2.daggerZero + " 1 - 9"
+            digit = gets.chomp
+            while !(array.include? digit.to_i)
+              puts "This cell is already taken, choose other cell:"
+              digit = gets.chomp
+            end
+            while array.include? digit.to_i
+              if array.include? digit.to_i
+                @@board.initBoard(digit, @@player2.daggerZero)
+                @@board.showBoard
+                array.delete(digit.to_i)
+              end
+            end
+          name = @@player2.name
+        end
+      end
+      puts name + " won !"
   end
 end
-
 game = Game.new
 game.initialising
